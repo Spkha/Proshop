7ef069a21bc5262206cda6593d25cde073b2aab1
Anupom Roy <anupomray@gmail.com>
2023-09-18 02:33:29 +0600

    Initial commit

---
+++ frontend/src/components/FormContainer.jsx
@@ -0,0 +1,16 @@
+import { Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+
+const FormContainer = ({ children }) => {
+  return (
+    <Container>
+        <Row className='justify-content-md-center'>
+            <Col xs={12} md={6}>
+                { children }
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default FormContainer;
